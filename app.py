3f620722789590cbb1259fffdc5dd404e782d39b
Elisa Pfau <elisa.m.pfau@gmail.com>
2022-07-20 22:31:25 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    color = os.environ.get('COLOR', "green") 
+    message = "This is a web scraping tool.  Ther purpose is to scrape the top 30 topic on Github, as well as the top 30 repositories on Github for each topic." 
+    return message
+
+@app.route('/scrape')
+def hello():
+    return main.py
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
